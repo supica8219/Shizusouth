01b0944ad50e5ad00e013bcf7f4baaa514066554
nakamura <shun29zw@gmail.com>
2021-08-29 02:44:13 +0900

    PC用

--- main.js
+++ main.js
@@ -1,64 +1,83 @@
+//定義
+var AINO,DAIGAKU,SHIZUOKA,HAMAMATU,DATE;
+var body=document.getElementById("body");
+Set_Datas();
+//関数---------------------------------------------------------------------
+function View_Bus(){
+  var div=document.createElement("div");
+  div.classList.add("content");
+  div.appendChild(View_Bus_Soon(AINO,"愛野駅発"));
+  div.appendChild(View_Bus_Soon(DAIGAKU,"大学発"));
+  body.appendChild(div);
+}
 
-var table=document.getElementById("tbody");
-var table2=document.getElementById("tbody2");
-var table3=document.getElementById("tbody3");
-var AINOTime=document.getElementsByClassName("AINOTime");
-var DAIGAKUTime=document.getElementsByClassName("DAIGAKUTime");
-var ShizuokaTime=document.getElementsByClassName("ShizuokaTime");
-var HamamatuTime=document.getElementsByClassName("HamamatuTime");
-var i,j,k;
-var AINO,DAIGAKU;
-
+function View_Bus_Soon(times,description){
+  var div=document.createElement("div");
+  var p=document.createElement("p");
+  p.innerHTML=description;
+  div.appendChild(p);
+  div.classList.add("bus-soon");
+  var count=0;
+  for(var i=0;i<times.length&&count<3;i++){
+    if(times[i*3]>DATE.getHours()){
+      var p=document.createElement("p");
+      p.innerHTML=times[i*3]+":"+String(times[i*2]).padStart(2, '0');
+      div.appendChild(p);
+      count++;
+    }
+  }
+  return div;
+}
+function Set_Datas(){
   var AD = new XMLHttpRequest();
   AD.addEventListener("load", Aino_To_Daigaku);
   AD.open("GET", "./AD.txt");
   AD.send();
-
   var DA = new XMLHttpRequest();
   DA.addEventListener("load", Daigaku_To_Aino);
   DA.open("GET", "./DA.txt");
   DA.send();
-
   var SH = new XMLHttpRequest();
   SH.addEventListener("load", Shizuoka_To_Hamamatu);
   SH.open("GET", "./SH.txt");
   SH.send();
-
   var HS = new XMLHttpRequest();
   HS.addEventListener("load", Hamamatu_To_Shizuoka);
   HS.open("GET", "./HS.txt");
   HS.send();
-
+  DATE = new Date();
+  HS.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        View_Bus();
+    }
+  }
+}
 
 function Aino_To_Daigaku () {
-  //console.log(this.responseText);
   AINO=this.responseText.split(",");
   for(i=0;i<AINO.length;i++){
      AINO[i]=AINO[i].replace('\r\n','');
   }
   console.log(AINO);
 }
+
 function Daigaku_To_Aino () {
-  //console.log(this.responseText);
   DAIGAKU=this.responseText.split(",");
   for(i=0;i<DAIGAKU.length;i++){
      DAIGAKU[i]=DAIGAKU[i].replace('\r\n','');
   }
-  console.log(DAIGAKU);
 }
+
 function Shizuoka_To_Hamamatu () {
-  //console.log(this.responseText);
-  Hamamatu=this.responseText.split(",");
-  for(i=0;i<Hamamatu.length;i++){
-     Hamamatu[i]=Hamamatu[i].replace('\r\n','');
+  HAMAMATU=this.responseText.split(",");
+  for(i=0;i<HAMAMATU.length;i++){
+    HAMAMATU[i]=HAMAMATU[i].replace('\r\n','');
   }
-  console.log(Hamamatu);
 }
+
 function Hamamatu_To_Shizuoka () {
-  //console.log(this.responseText);
-  Shizuoka=this.responseText.split(",");
-  for(i=0;i<Shizuoka.length;i++){
-     Shizuoka[i]=Shizuoka[i].replace('\r\n','');
+  SHIZUOKA=this.responseText.split(",");
+  for(i=0;i<SHIZUOKA.length;i++){
+    SHIZUOKA[i]=SHIZUOKA[i].replace('\r\n','');
   }
-  console.log(Shizuoka);
 }
